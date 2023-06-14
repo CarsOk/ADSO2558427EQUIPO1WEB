f0098acd1790f090644cdc99420f9c8057368bd7
Luis Enrique Junior Lemus Duque <Luislemus0109@gmail.com>
2023-06-14 16:12:48 -0500

    Update home_controller.rb

--- proyecto/proyecto2/Proyecto1/app/controllers/home_controller.rb
+++ proyecto/proyecto2/Proyecto1/app/controllers/home_controller.rb
@@ -1,11 +1,9 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
- def index
-     @mainTitle = "Welcome to Inspinia Rails Seed Project"
-     @mainDesc = "It is an application skeleton for a typical Ruby on Rails web app. You can use it to quickly bootstrap your webapp projects and dev/prod environment."
-   end
+  def index
+  end
 
-   def minor
-   end
+  def minor
+  end
 
 end
