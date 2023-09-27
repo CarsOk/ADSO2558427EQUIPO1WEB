a8e46a36f36604cec3d20e1437bf9c8361f8f240
Wilmer Carmona <wilmercarmona810@gmail.com>
2023-09-27 14:13:35 -0500

    update

--- proyecto/proyecto3/app/helpers/application_helper.rb
+++ proyecto/proyecto3/app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def is_active_controller(*controllers)
+        controllers.include?(controller_name) ? 'active' : ''
+      end
+    
+      def is_active_action(*actions)
+        actions.include?(action_name) ? 'active' : ''
+      end
 end
