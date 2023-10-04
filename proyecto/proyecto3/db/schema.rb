4278b3e6427c3557bab21e7246f47229b25e51f4
Luis Enrique Junior Lemus Duque <Luislemus0109@gmail.com>
2023-10-03 22:33:03 -0500

    update

--- proyecto/proyecto3/db/schema.rb
+++ proyecto/proyecto3/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_03_031246) do
+ActiveRecord::Schema.define(version: 2023_10_04_025918) do
 
   create_table "order_items", force: :cascade do |t|
     t.integer "quantity"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2023_10_03_031246) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+    t.string "description"
+    t.boolean "available"
   end
 
   create_table "users", force: :cascade do |t|
