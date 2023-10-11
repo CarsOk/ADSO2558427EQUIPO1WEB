c61557a0af717cd359300020605e56a37c602836
Luis Enrique Junior Lemus Duque <Luislemus0109@gmail.com>
2023-10-10 22:52:20 -0500

    update

--- proyecto/proyecto3/db/schema.rb
+++ proyecto/proyecto3/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_04_025918) do
+ActiveRecord::Schema.define(version: 2023_10_11_023926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2023_10_04_025918) do
     t.string "image"
     t.string "description"
     t.boolean "available"
+    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|
