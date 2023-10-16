1b7af0b54049f80fd06107a3427d57d972487625
Wilmer Carmona <wilmercarmona810@gmail.com>
2023-10-15 22:53:44 -0500

    update

--- proyecto/proyecto3/app/controllers/application_controller.rb
+++ proyecto/proyecto3/app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
     include ApplicationHelper
-end
+    include Pundit::Authorization
+    def user
+      @user = current_user
+    end
+  end
+  
