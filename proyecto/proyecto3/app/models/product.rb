0190661ed62b776738d4f43b91b1394cb70908fb
Luis Enrique Junior Lemus Duque <Luislemus0109@gmail.com>
2023-10-20 14:49:12 -0500

    update

--- proyecto/proyecto3/app/models/product.rb
+++ proyecto/proyecto3/app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ApplicationRecord
     has_many :order_items
     mount_uploader :image, ImageUploader
     validates :title, :price, :image, :description, :category, presence: true
+    validates :title, :price, :description, :category, presence: true, if: -> { json_request? }
+
+    private
+  
+    def json_request?
+      request.format.json?
+    end
 end
