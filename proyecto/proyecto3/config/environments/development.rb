4600fd70d1fb698daa5ea1721d01405dce5e98c4
Luis Enrique Junior Lemus Duque <Luislemus0109@gmail.com>
2023-10-20 15:07:26 -0500

    Update development.rb

--- proyecto/proyecto3/config/environments/development.rb
+++ proyecto/proyecto3/config/environments/development.rb
@@ -75,5 +75,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.hosts << "f7aa-186-98-192-244.ngrok-free.app"
+  config.hosts << "5509-186-98-192-244.ngrok-free.app"
 end
