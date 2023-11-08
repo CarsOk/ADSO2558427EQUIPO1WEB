e10c469c3f40887f785dae149a1668c8f352e650
Luis Enrique Junior Lemus Duque <Luislemus0109@gmail.com>
2023-11-08 11:26:28 -0500

    Update development.rb

--- proyecto/proyecto3/config/environments/development.rb
+++ proyecto/proyecto3/config/environments/development.rb
@@ -75,6 +75,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.hosts << "ba0e-152-201-190-135.ngrok-free.app"
+  config.hosts << "15a4-152-204-157-218.ngrok-free.app"
   config.middleware.use WickedPdf::Middleware
 end
