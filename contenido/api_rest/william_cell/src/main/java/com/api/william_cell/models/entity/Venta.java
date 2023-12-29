049c7dd2a9f69798efa0584c11a9b8c7c2041e74
Emanuel Obredor <110873019+EmanuelObredor18@users.noreply.github.com>
2023-12-29 15:16:36 -0500

    Update Venta.java

--- contenido/api_rest/william_cell/src/main/java/com/api/william_cell/models/entity/Venta.java
+++ contenido/api_rest/william_cell/src/main/java/com/api/william_cell/models/entity/Venta.java
@@ -2,13 +2,11 @@ package com.api.william_cell.models.entity;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
