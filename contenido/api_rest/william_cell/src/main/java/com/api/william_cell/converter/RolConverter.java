f44521fb302714bbf588536ab7f889f1277c30ac
Emanuel Obredor <110873019+EmanuelObredor18@users.noreply.github.com>
2024-01-02 16:32:53 -0500

    Quitar importaciones innecesarias

--- contenido/api_rest/william_cell/src/main/java/com/api/william_cell/converter/RolConverter.java
+++ contenido/api_rest/william_cell/src/main/java/com/api/william_cell/converter/RolConverter.java
@@ -1,14 +1,10 @@
 package com.api.william_cell.converter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Component;
 
 import com.api.william_cell.converter.interfaces.EntityConverter;
 import com.api.william_cell.models.dto.RolDto;
 import com.api.william_cell.models.entity.Rol;
-import com.api.william_cell.models.entity.Usuario;
 
 @Component("rolConverter")
 public class RolConverter implements EntityConverter<Rol, RolDto> {
@@ -19,13 +15,9 @@ public class RolConverter implements EntityConverter<Rol, RolDto> {
             return null;
         }
 
-        List<Usuario> usuarios = new ArrayList<>();
-
-
         return Rol.builder()
             .rol_id(dto.getRol_id())
             .rol_nombre(dto.getRol_nombre())
-            .usuarios(usuarios)
             .build();
     }
 
